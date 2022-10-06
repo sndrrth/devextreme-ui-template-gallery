355c6e6e9781f9f0cc2bc6051c289005c7e3bb27
Sandro Roth <sandro.roth@swissgrc.com>
2022-10-06 11:36:16 +0200

    Style sidenav groups

--- packages/angular/src/app/app-navigation.ts
+++ packages/angular/src/app/app-navigation.ts
@@ -1,50 +1,54 @@
 export const navigation = [
   {
     text: 'CRM',
-    icon: 'user',
     path: '',
     items: [
       {
         text: 'Contact List',
         path: '/crm-contact-list',
+        icon: 'user',
       },
       {
         text: 'Contact Details',
         path: '/crm-contact-details',
+        icon: 'edit',
       },
     ],
   },
   {
     text: 'Planning',
-    icon: 'event',
     path: '',
     items: [
       {
         text: 'Task List',
         path: '/planning-task-list',
+        icon: 'event',
       },
       {
         text: 'Task Details',
         path: '/planning-task-details',
+        icon: 'edit',
       },
     ],
   },
   {
     text: 'Analytics',
-    icon: 'chart',
     path: '',
     items: [
       {
         text: 'Dashboard',
         path: '/analytics-dashboard',
+        icon: 'chart',
       },
       {
         text: 'Sales Report',
         path: '/analytics-sales-report',
+        icon: 'money',
       },
       {
         text: 'Geography',
         path: '/analytics-geography',
+        icon: 'globe',
       },
     ],
   },
