8c30c31ee8776e1fd6e98da1373f0ae67fd586b5
Sandro Roth <sandro.roth@swissgrc.com>
2023-01-06 15:58:07 +0100

    Style badges

--- packages/angular/src/app/shared/components/contract-status/contract-status.component.ts
+++ packages/angular/src/app/shared/components/contract-status/contract-status.component.ts
@@ -3,11 +3,17 @@ import {
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactStatus } from 'src/app/shared/types/contact';
+import { faCheckCircle, } from '@fortawesome/pro-regular-svg-icons';
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'contract-status',
   template: `
-  <span class="{{ input ? 'input' : '' }} status status-{{ statusMapping[value] }}">{{ value }}</span>
+  <span class="{{ input ? 'input' : '' }} status status-{{ statusMapping[value] }}">
+    <span [innerHTML]="iconMapping[value]"></span>
+    {{ value }}
+  </span>
 `,
   styleUrls: ['./contract-status.component.scss'],
 })
@@ -22,6 +28,16 @@ export class ContractStatusComponent {
     'Entwurf': 'entwurf',
     'Prüfung': 'pruefung',
   };
+
+  readonly iconMapping: Record<string, any> = {
+    'Abgelehnt': this.sanitizer.bypassSecurityTrustHtml(icon(faCheckCircle).html[0]),
+    'Aktiv': this.sanitizer.bypassSecurityTrustHtml(icon(faCheckCircle).html[0]),
+    'Entwurf': this.sanitizer.bypassSecurityTrustHtml(icon(faCheckCircle).html[0]),
+    'Prüfung': this.sanitizer.bypassSecurityTrustHtml(icon(faCheckCircle).html[0]),
+  };
+
+
+  constructor(private sanitizer: DomSanitizer) {}
   
 }
 
